4e67e882a50e8b0514edeb1c98125e71c3819cf3
k0v4 <vadkos33@outlook.com>
2024-02-18 09:43:13 +0300

    Epoll edit

--- part_59/epoll/epoll.c
+++ part_59/epoll/epoll.c
@@ -5,7 +5,10 @@
 int main(void)
 {
     struct epoll_event event;
+    struct epoll_event events[5];
     int epoll_fd = epoll_create1(0); /* stdin event */
+    int len_rd;
+    char buf[4096];
     if (epoll_fd == -1)
         return 1;
     
@@ -17,19 +20,21 @@ int main(void)
         return 1;
     }
 
-
-
-    if (close(epoll_fd)) {
-        printf("Failed close()\n");
-        return 1;
-    }
-
     /* Count of epoll() events */
     int event_count = epoll_wait(epoll_fd, &event, 1, 5000);
     if (!event_count) {
         printf("Timeout\n");
     } else {
-        printf("Timeout\n");
+        for (int i = 0; i < event_count; i++) {
+            printf("Handling file descriptor: %u\n", events[i].data.fd);
+            len_rd = read(events[i].data.fd, buf, sizeof(buf));
+            buf[len_rd] = '\0';
+            printf("%s", buf);
+        }
+    }
+
+    if (close(epoll_fd)) {
+        printf("Failed to close\n");
         return 1;
     }
 
